4dda9ee7cfd93009e5e910bd65e5b47654d369b8
Stefanie Kobsar Rodríguez <stkobsar7@gmail.com>
2020-11-07 23:54:46 +0100

    use get csv to get information using another stats url

--- scrapping_basket/main.py
+++ scrapping_basket/main.py
@@ -1,12 +1,21 @@
 import scrapping_basket.get_nba_csv as gc
+import scrapping_basket.get_teams as gt
 
 
-def main(year):
-    gc.get_nba_csv(year)
+def main(url):
+
+    gc.get_nba_csv(url)
+
+
+    #gt.getTeamsURL(url_teams)
 
 
 if __name__ == "__main__":
 
     year = 2019
-    main(year)
+    url = f"https://www.basketball-reference.com/leagues/NBA_{year}_per_game.html"
+    url_teams_wins = "https://www.basketball-reference.com/leagues/NBA_wins.html"
+
+
+    main(url_teams_wins)
 
